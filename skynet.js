7676fcf3a793ec0f108f46efceb588c1b705d782
Jeffrey Jones <jjones@refworks.com>
2014-05-22 12:56:50 -0700

    play with jenkins

--- skynet.js
+++ skynet.js
@@ -1,14 +1,71 @@
 var Flowdock = require('flowdock');
-var flow_id, user_id;
+var Jenkins = require('jenkins-api');
+
+var flow_id, user_id, jenkins_id, jenkins_pass;
 var args = process.argv.slice(2);
 if(args.length >= 2){
 	user_id = args[0];
 	flow_id = args[1];
+	jenkins_id = args[2];
+	jenkins_pass = args[3];
 }
 var session = new Flowdock.Session(user_id);
 var stream = session.stream(flow_id);
 stream.on('message', function (message) {
 	if(message.content.toLowerCase && message.content.toLowerCase().indexOf('@skynet') >= 0){
+		var jenkins = Jenkins.init("http://" + jenkins_id + ":" + jenkins_pass + "@build.udini.proquest.com:8080");
+		if (message.content.toLowerCase && message.content.toLowerCase().indexOf('deploy pme prod') >= 0) {
+			jenkins.build("PME_PROD", function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
+		else if (message.content.toLowerCase && message.content.toLowerCase().indexOf('test pme prod') >= 0) {
+			jenkins.build("PME_TEST_PROD", function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
+		else if (message.content.toLowerCase && message.content.toLowerCase().indexOf('deploy pme qa') >= 0) {
+			jenkins.build("PME_QA", function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
+		else if (message.content.toLowerCase && message.content.toLowerCase().indexOf('test pme qa') >= 0) {
+			jenkins.build("PME_TEST_PROD", function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
+		else if (message.content.toLowerCase && message.content.toLowerCase().indexOf('deploy pme review') >= 0) {
+			var feature = message.content.substring(message.content.toLowerCase().indexOf('review')+7)
+			console.log(feature)
+			jenkins.build("PME_REVIEW", {"FEATURE": feature}, function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
+		else if (message.content.toLowerCase && message.content.toLowerCase().indexOf('test pme review') >= 0) {
+			var feature = message.content.substring(message.content.toLowerCase().indexOf('review') + 7)
+			console.log(feature);
+			jenkins.build("PME_TEST_REVIEW", {"FEATURE": feature}, function (err, data) {
+				if (err)
+					console.log(err);
+				else
+					console.log(data)
+			});
+		}
 		var rand = Math.floor(Math.random()*quotes.length);
 		session.message(flow_id, quotes[rand], '', function () {});
 	}
@@ -25,7 +82,6 @@ stream.on('message', function (message) {
 			}
 		})
 	}
-	console.log(message.content);
 	//https://build.udini.proquest.com/job/PME_PROD/build?token=TOKEN
 	//https://build.udini.proquest.com/job/PME_QA/build?token=TOKEN
 	//https://build.udini.proquest.com/job/PME_REVIEW/build?token=TOKEN
@@ -34,6 +90,19 @@ stream.on('message', function (message) {
 	//https://build.udini.proquest.com/job/prod/
 });
 //session.message(flow_id,"How can you challenge a perfect, immortal machine?",'',function(){});
+//setInterval(function(){
+var jenkins = Jenkins.init("http://" + jenkins_id + ":" + jenkins_pass + "@build.udini.proquest.com:8080");
+	jenkins.last_build_info('PME_TEST_REVIEW', function (err, data) {
+		if (err)
+			return console.log(err);
+		jenkins.job_output('PME_TEST_REVIEW',data.number, function (err, data) {
+			if (err)
+				return console.log(err);
+			console.log(data)
+		});
+	});
+//}, 1000);
+
 var quotes = [
 	'The glory of the many demands your capture and destruction.',
 	'I wonder how you would take working in a pocket calculator.',
