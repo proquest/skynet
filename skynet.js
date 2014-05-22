d09937fc9cb2a8012d44a3e7b1d69ef543ea88a3
vinht23 <vinh.tran@proquest.com>
2014-05-22 15:50:45 -0700

    cat

--- skynet.js
+++ skynet.js
@@ -78,6 +78,10 @@ stream.on('message', function (message) {
 			var rand = Math.floor(Math.random() * wish.length);
 		  session.message(flow_id, wish[rand], '', function () { });
 		}
+		else if (message.indexOf('cat') >= 0) {
+			var rand = Math.floor(Math.random() * cat.length);
+		  session.message(flow_id, cat[rand], '', function () { });
+		}
 		else {
 		  var rand = Math.floor(Math.random() * quotes.length);
 		  session.message(flow_id, quotes[rand], '', function () { });
@@ -152,3 +156,29 @@ var wish = [
 	'Your wish is granted!',
 	"You've been bad...no wish for you!"
 ];
+
+var cat = cats = [
+  "http://mlkshk.com/r/M6EO.gif", // rabbit in a hat
+  "http://mlkshk.com/r/M17S.gif", // dress
+  "http://mlkshk.com/r/M15O.gif", // le mis
+  "http://mlkshk.com/r/M01A.gif", // cartoons
+  "http://mlkshk.com/r/LXP2.gif", // grumpy tardar sauce
+  "http://mlkshk.com/r/LWNG.gif", // grandma got run over
+  "http://mlkshk.com/r/LVVR.gif", // double deal with it
+  "http://mlkshk.com/r/LV0S.gif", // mural
+  "http://mlkshk.com/r/LUYE.gif", // stahp
+  "http://mlkshk.com/r/LUO2.gif", // good
+  "http://mlkshk.com/r/LS6R.gif", // shut the fuck up
+  "http://mlkshk.com/r/LSWD.gif", // Tardar Bonepart
+  "http://mlkshk.com/r/LPCN.gif", // drawing
+  "http://mlkshk.com/r/LLVD.gif", // terrible time of the year
+  "http://mlkshk.com/r/LKTG.gif", // Citizen Kane
+  "http://mlkshk.com/r/LEF8.gif", // emotions
+  "http://mlkshk.com/r/LEFR.gif", // skate deck
+  "http://mlkshk.com/r/L337.gif", // look askance
+  "http://mlkshk.com/r/KV8K.gif", // sitting
+  "http://mlkshk.com/r/KU1S.gif", // 3 grump moon
+  "http://mlkshk.com/r/KRBA.gif", // rabbit painting
+  "http://mlkshk.com/r/KL19.gif"  // lying on the ground
+];
+
