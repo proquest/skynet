0dff6603d1de4c10c978b340a11d09cd9f900407
Jeffrey Jones <jjones@refworks.com>
2014-07-13 10:06:01 -0700

    trello upgrades

--- skynet.js
+++ skynet.js
@@ -1,21 +1,115 @@
 var Flowdock = require('flowdock');
 var Jenkins = require('jenkins-api');
+var Trello = require('node-trello');
 
-var flow_id, user_id, jenkins_id, jenkins_pass, jenkins_server = 'build.udini.proquest.com';
+var flow_id, user_id, jenkins_id, jenkins_pass, jenkins_server = 'build.udini.proquest.com', trello_key, trello_token;
 var args = process.argv.slice(2);
 if(args.length >= 2){
 	user_id = args[0];
 	flow_id = args[1];
 	jenkins_id = args[2];
 	jenkins_pass = args[3];
-	if(args.length == 5)
-		jenkins_server = args[3];
+  trello_key = args[4];
+  trello_token = args[5];
+	if(args.length == 7)
+		jenkins_server = args[6];
 }
 var jenkins = Jenkins.init("http://" + jenkins_id + ":" + jenkins_pass + "@"+ jenkins_server+":8080");
 var testsQueued = [];
 var session = new Flowdock.Session(user_id);
 var stream = session.stream(flow_id);
 var lastMessage = (new Date()).valueOf();
+
+var trello = new Trello(trello_key, trello_token);
+var all = ["50abd8311448f9bb320124b7"/*Nicky*/, "5249a3f58439b14f0b004ba6"/*Eric*/, "50b79366d64660800100070f"/*Jeff*/, "5253045c3bb54f4739007976"/*Caistarrin*/, "50c76a8e1a9624e36901cd5e"/*Alla*/, "50c617c2de78ae52360135d7"/*Jay*/, "50a12a19b906c4b13402c90b"/*Vinh*/, "50a12a7c61f044950e00234e"/*Airaka*/],
+  owners = ["50abd8311448f9bb320124b7"/*Nicky*/, "5249a3f58439b14f0b004ba6"/*Eric*/],
+  developers = ["50b79366d64660800100070f"/*Jeff*/, "5253045c3bb54f4739007976"/*Caistarrin*/, "50c76a8e1a9624e36901cd5e"/*Alla*/, "50c617c2de78ae52360135d7"/*Jay*/],
+  qa = ["50a12a19b906c4b13402c90b"/*Vinh*/, "50a12a7c61f044950e00234e"/*Airaka*/],
+  done = ["50c5130cc583e4a46f001572", "52b097968685fefe5200aa62", "50c51318c583e4a46f001579", "50c51325c583e4a46f001580", "53a06a7365d3b91cf4bbdafe"],
+  name = {"50abd8311448f9bb320124b7":"Nicky", "5249a3f58439b14f0b004ba6":"Eric","50a12a19b906c4b13402c90b":"Vinh", "50a12a7c61f044950e00234e":"Airaka", "50b79366d64660800100070f":"Jeff", "5253045c3bb54f4739007976":"Caistarrin", "50c76a8e1a9624e36901cd5e":"Alla", "50c617c2de78ae52360135d7":"Jay"}
+  devRatio = 2 / 3,
+  startDate = new Date("6/9/2014"),
+  sprintPayout = 1000,
+  sprintDuration = 14,
+  ownerPayout = (new Date() - startDate),
+  totalOwnerBank = Math.floor(ownerPayout / (1000 * 60 * 60 * 24 * sprintDuration)) * sprintPayout,
+  membersBounty = {};
+function getBounties(callback){
+  var asyncStack = ["board"];
+  trello.get("1/board/slcwg4g9/lists", { cards: "open" }, function (err, data) {
+    asyncStack.pop()
+    if (err) throw err;
+    for (var i = 0; i < data.length; i++) {
+      for (var j = 0; j < data[i].cards.length; j++) {
+        var card = data[i].cards[j];
+        asyncStack.push("cards")
+        trello.get("1/cards/" + card.id, { actions: "commentCard" }, function (err, comments) {
+          asyncStack.pop()
+          if (err) throw err;
+          var thisBounty = 0;
+          for (var k = 0; k < comments.actions.length; k++) {
+            var comment = comments.actions[k].data.text.toLowerCase(),
+              bountyRegex = /[^a-zA-Z]\d|^\d/,
+              bounty = comment.indexOf("agree") == -1 && comment.length < 100 && bountyRegex.test(comment) ? parseInt(comment.replace(/[^\d]/g, "")) : undefined;
+
+            if (bounty && all.indexOf(comments.actions[k].idMemberCreator) != -1) {
+              thisBounty += bounty;
+              var id = comment.indexOf("bug") >= 0 ? "skynet" : comments.actions[k].idMemberCreator,
+                obj = membersBounty[id];
+
+              if (obj) {
+                obj.flash += bounty;
+
+              }
+              else {
+                membersBounty[id] = {"flash": bounty, "awarded": (owners.indexOf(id) >= 0 ? totalOwnerBank : 0)};
+              }
+            }
+          }
+          if (thisBounty && done.indexOf(comments.idList) >= 0) {
+            awardBounty(comments.idMembers, thisBounty, comments.name);
+          }
+          if(asyncStack.length == 0){
+            var winners = [{name:"@Skynet",bounty:"∞"}]//&infin;
+            for(var key in membersBounty){
+              winners.push({id:key, name:"@"+name[key],bounty:membersBounty[key].awarded,spent: membersBounty[key].flash});
+            }
+            callback(winners);
+          }
+        });
+      }
+    }
+  });
+  function awardBounty(members, bounty, title) {
+    var developerCount = 0, qaCount = 0;
+    for (var i = 0; i < members.length; i++) {
+      if (developers.indexOf(members[i]) >= 0)
+        developerCount++;
+      if (qa.indexOf(members[i]) >= 0)
+        qaCount++;
+    }
+    for (var i = 0; i < members.length; i++) {
+      if (developers.indexOf(members[i]) >= 0 || qa.indexOf(members[i]) >= 0) {
+        var award = amount(members[i], bounty, (developers.indexOf(members[i]) >= 0 ? developerCount : qaCount));
+        if (membersBounty[members[i]]) {
+          membersBounty[members[i]].awarded += award;
+        }
+        else {
+          membersBounty[members[i]] = {"flash": 0, "awarded": award, "fullName": members[i]};
+        }
+        console.log(name[members[i]] +" "+award+" "+title)
+      }
+    }
+  }
+
+  function amount(member, bounty, count) {
+    var isDeveloper = developers.indexOf(member) >= 0;
+    var devBounty = Math.ceil(bounty * devRatio);
+    var qaBounty = Math.floor(bounty * (1 - devRatio));
+    var bountyResult = isDeveloper ? devBounty : qaBounty;
+    return bountyResult / count;
+  }
+}
 
 function getParentId(message){
 	if(message.content.text && message.tags.length > 0){
@@ -35,7 +129,22 @@ function processMessage(message) {
     var messageContent = originalMessage.toLowerCase ? originalMessage.toLowerCase() : "";
     if (messageContent.indexOf('@skynet') >= 0) {
       var rand = Math.random()
-      if (messageContent.indexOf('love') >= 0) {
+      if(messageContent.indexOf("who's winning?")){
+
+        //@skynet who's winning?
+        //@skynet who's winning this sprint?
+        getBounties(function(data){
+          data.sort(function (a, b) {
+            return b.bounty - a.bounty;
+          })
+          for(var i = 0; i < data.length; i++){
+            if(developers[data[i].id] || qa[data[i].id])
+              session.comment(flow_id, parentId, data[i].name+": "+data[i].bounty, '', function () {});
+          }
+        });
+        //@skynet who has flash?
+      }
+      else if (messageContent.indexOf('love') >= 0) {
         session.comment(flow_id, parentId, ':heartpulse:', '', function () {
         });
       }
