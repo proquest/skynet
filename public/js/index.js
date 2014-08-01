c646baba9621a3fbf4949eff41479f46cad33aa6
Jeffrey Jones <jjones@refworks.com>
2014-08-01 09:03:12 -0700

    skynet debug

--- public/js/index.js
+++ public/js/index.js
@@ -8,27 +8,10 @@ angular.module('skynet.controllers', []).
     $scope.todayOnly = true;
     $scope.now = new Date();
     $scope.filterToday = function(date){
-      console.log(date);
       return function(error) {
-        console.log(error);
         var day = 24 * 60 * 60 * 1000;
         return !$scope.todayOnly || error.timestamp >= (date - day);
       }
-    };
-    $scope.handleDates = function(dates){
-      var newDates = {}
-      for(var i = 0; i < dates.length; i++){
-        console.log(dates[i].getDate());
-        var dateString = dates[i].toDateString();
-        if(newDates[dateString])
-          newDates[dateString]++;
-        else
-          newDates[dateString] = 1;
-      }
-      var output = [];
-      for(var date in newDates)
-        output.push({date:date,count:newDates[date]})
-      return output;
     };
     $http({
       method: 'GET',
@@ -37,33 +20,23 @@ angular.module('skynet.controllers', []).
       success(function (data, status, headers, config) {
         var indexes = {}
         $scope._uniques = [];
+        var nowString = $scope.now.toDateString();
         angular.forEach(data, function (value, key) {
+          if (value.timestamp)
+            value.timestamp = new Date(value.timestamp);
+          value.todayCount = value.timestamp && nowString == value.timestamp.toDateString() ? 1 : 0;
           value.count = 1;
-          var date = new Date(value.timestamp);
-          if(value.file) {
-            if (indexes[value.file + value.lineNumber] >= 0) {
-              $scope._uniques[indexes[value.file + value.lineNumber]].count++;
-              $scope._uniques[indexes[value.file + value.lineNumber]].timestamp.push(new Date(value.timestamp));
-            }
-            else {
-              indexes[value.file + value.lineNumber] = $scope._uniques.length;
-              value.timestamp = [new Date(value.timestamp)];
-              $scope._uniques.push(value);
-            }
+          var key = value.file ? value.file + value.lineNumber : value.func;
+          if (indexes[key] >= 0) {
+            $scope._uniques[indexes[key]].count++;
+            $scope._uniques[indexes[key]].todayCount += value.todayCount;
           }
-          else if(value.func) {
-            if(indexes[value.func] >= 0) {
-              $scope._uniques[indexes[value.func]].count++;
-              $scope._uniques[indexes[value.func]].timestamp.push(new Date(value.timestamp));
-            }
-            else{
-              indexes[value.func] = $scope._uniques.length;
-              value.timestamp = [new Date(value.timestamp)];
-              $scope._uniques.push(value);
-            }
+          else {
+            indexes[key] = $scope._uniques.length;
+            $scope._uniques.push(value);
           }
         });
-        $scope._uniques = $scope._uniques.sort(function(a,b){return a.count > b.count});
+        $scope._uniques = $scope._uniques.sort(function(a,b){return a.count == b.count ? 0 : a.count > b.count ? -1 : 1});
       }).
       error(function (data, status, headers, config) {
         $scope._error = 'Error!';
